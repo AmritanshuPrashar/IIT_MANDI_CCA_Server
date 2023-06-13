c839e4f79696669e46fd3abb75cf9caf02b82b53
Amritanshu <amritanshu1002@gmail.com>
2023-06-13 15:18:30 +0530

    hello

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 const express = require('express')
-
+const mongoose = require('mongoose');
 const app = express()
 const PORT = 4000
 
+
+mongoose.connect("mongodb+srv://amri07:amri07@cluster0.oomopds.mongodb.net/?retryWrites=true&w=majority", {
+    dbName: "IITMandi_Project",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  db.once('open', () => {
+    console.log('Connected to MongoDB');
+  });
+
+
+  const sensorSchema = new mongoose.Schema({
+    timestamp: {
+      type: Date,
+      default: Date.now()
+    },
+    data: {
+      temperature: {
+        internal: {
+          type: String
+        },
+        external: {
+          type: String
+        }
+      },
+      pressure: {
+        internal: {
+          type: String
+        },
+        external: {
+          type: String
+        }
+      },
+      humidity: {
+        internal: {
+          type: String
+        },
+        external: {
+          type: String
+        }
+      }
+    }
+  });
+  
+const Sensor = mongoose.model('Sensor', sensorSchema);
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
+    console.log(`API listening on PORT ${PORT} `)
 })
 
+app.get('/data', async (req, res) => {
+    let data = await Sensor.find();
+    res.json({
+        data: data
+    })
+})
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
