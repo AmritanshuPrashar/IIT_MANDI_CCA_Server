8624c79ef58fdd883868e72efce972d25955318d
Amritanshu <amritanshu1002@gmail.com>
2023-06-24 20:26:33 +0530

    Minor bug fixes for signup and login

--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     email: {
 
         type: String,
+        unique: true,
         required: 'Email address is required',
         validate: [validateEmail, 'Please fill a valid email address'],
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
